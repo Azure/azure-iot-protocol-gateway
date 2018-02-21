7df089cbd932c25f2599b96b2592553892f48613
Max Gortman <mgortman@microsoft.com>
2018-02-21 10:44:07 -0800

    upgrade to DotNetty 0.4.7

---
+++ test/ProtocolGateway.Tests/Extensions/ByteBufferExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.Azure.Devices.ProtocolGateway.Tests.Extensions
+{
+    using DotNetty.Buffers;
+    using DotNetty.Common.Utilities;
+    using System;
+
+    public static class ByteBufferExtensions
+    {
+        public static byte[] ToArray(this IByteBuffer buffer)
+        {
+            if (!buffer.IsReadable())
+            {
+                return ArrayExtensions.ZeroBytes;
+            }
+            var segment = buffer.GetIoBuffer();
+            var result = new byte[segment.Count];
+            Array.Copy(segment.Array, segment.Offset, result, 0, segment.Count);
+            return result;
+        }
+    }
+}
